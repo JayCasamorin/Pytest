f660ba10425d400827136b33a27c45ed8e091931
Jay Casamorin <jay.casamorin@gmail.com>
2026-08-09 11:39:08 +0800

    Added Chapter 4 - Parametrized Test Cases

--- tests/test_math.py
+++ tests/test_math.py
@@ -14,3 +14,16 @@ def test_divide_by_zero():
         num = 1 / 0
 
     assert 'division by zero' in str(e.value)
+
+products = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
